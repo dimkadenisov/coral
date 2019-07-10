cd95c3f9a1b5031cea1879df176b92ad3b86985e
Dima Denisov <2denisov@bk.ru>
2019-07-11 01:25:32 +0300

    Первичная реализация хедера

---
+++ frontend/scripts/components/header.js
@@ -0,0 +1,19 @@
+const changeHeaderState = () => {
+  $('body').toggleClass('overflow_hidden');
+  $('.header-line').toggleClass('header-line_opened');
+  $('.header__content').toggleClass('header__content_opened');
+  $('.site-sections').toggleClass('site-sections_opened');
+  $('.top-line').toggleClass('top-line_opened');
+  $('.contacts-burger').toggleClass('contacts-burger_opened');
+};
+
+const changeSubMenuState = (event) => {
+  event.preventDefault();
+  $(event.currentTarget).toggleClass('site-sections__link_opened');
+  $('.header .overlay_white').toggleClass('d-block');
+}
+
+
+$('.burger-menu__checkbox').click(changeHeaderState);
+
+$('.site-sections__link:not(:only-child').click(changeSubMenuState);
