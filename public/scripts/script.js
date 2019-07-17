46b7486590ecab51442a29d9858a7b20ec3e2824
Dima Denisov <2denisov@bk.ru>
2019-07-17 18:11:13 +0300

    Новая сборка

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -69,6 +69,8 @@
   breakpointChecker();
 })();
 
+'use strict';
+
 var openBurgerMenu = function openBurgerMenu() {
   $('body').addClass('overflow_hidden');
   $('.header+.overlay').addClass('d-block');
@@ -98,12 +100,14 @@ var openSubMenu = function openSubMenu(event) {
   event.preventDefault();
   $(event.target).addClass('site-sections__link_opened');
   $(event.target).closest('.site-sections__list').children('.site-sections__list-item').addClass('d-none');
+  $(event.target).next().addClass('d-block');
   $(event.target).parent().removeClass('d-none');
   $('.top-line').addClass('d-none');
 };
 
 var closeSubMenu = function closeSubMenu() {
   $('.site-sections__link_opened').closest('.site-sections__list').children('.site-sections__list-item').removeClass('d-none');
+  $('.site-sections__link_opened').next().removeClass('d-block');
   $('.site-sections__link_opened').removeClass('site-sections__link_opened');
   $('.top-line').removeClass('d-none');
 };
@@ -115,11 +119,28 @@ var toggleSubMenuState = function toggleSubMenuState(event) {
 
 var addScrollHeader = function addScrollHeader() {
   $('.header').addClass('header_scrolled');
+  $('.header__content').addClass('header__content_scrolled');
+  $('.phone-block__description').addClass('d-none');
+  $('.basket__description').addClass('d-none');
+  $('.burger-menu').addClass('d-block');
+  $('.header-line__content').addClass('header-line__content_scrolled');
+  $('.site-sections').addClass('site-sections_scrolled');
+  $('.top-line').addClass('top-line_scrolled');
+  $('.header-line__logo').addClass('d-none');
+  $('.header-line__logo_scrolled').removeClass('d-none');
 };
 
 var removeScrollHeader = function removeScrollHeader() {
   $('.header').removeClass('header_scrolled');
-  console.log('hello');
+  $('.header__content').removeClass('header__content_scrolled');
+  $('.phone-block__description').removeClass('d-none');
+  $('.basket__description').removeClass('d-none');
+  $('.burger-menu').removeClass('d-block');
+  $('.header-line__content').removeClass('header-line__content_scrolled');
+  $('.site-sections').removeClass('site-sections_scrolled');
+  $('.top-line').removeClass('top-line_scrolled');
+  $('.header-line__logo').removeClass('d-none');
+  $('.header-line__logo_scrolled').addClass('d-none');
 };
 
 var toggleScrollHeader = function toggleScrollHeader() {
