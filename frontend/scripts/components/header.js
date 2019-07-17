e9fc570aae72c23b9d4f1eb3e0bb701a8ef76c32
Dima Denisov <2denisov@bk.ru>
2019-07-17 18:10:36 +0300

    Реализован фикс-хедер при скролле

--- frontend/scripts/components/header.js
+++ frontend/scripts/components/header.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const openBurgerMenu = () => {
   $('body').addClass('overflow_hidden');
   $('.header+.overlay').addClass('d-block');
@@ -28,12 +30,14 @@ const openSubMenu = (event) => {
   event.preventDefault()
   $(event.target).addClass('site-sections__link_opened');
   $(event.target).closest('.site-sections__list').children('.site-sections__list-item').addClass('d-none');
+  $(event.target).next().addClass('d-block');
   $(event.target).parent().removeClass('d-none');
   $('.top-line').addClass('d-none');
 };
 
 const closeSubMenu = () => {
   $('.site-sections__link_opened').closest('.site-sections__list').children('.site-sections__list-item').removeClass('d-none');
+  $('.site-sections__link_opened').next().removeClass('d-block');
   $('.site-sections__link_opened').removeClass('site-sections__link_opened');
   $('.top-line').removeClass('d-none');
 };
@@ -45,11 +49,28 @@ const toggleSubMenuState = (event) => {
 
 const addScrollHeader = () => {
   $('.header').addClass('header_scrolled');
+  $('.header__content').addClass('header__content_scrolled');
+  $('.phone-block__description').addClass('d-none');
+  $('.basket__description').addClass('d-none');
+  $('.burger-menu').addClass('d-block');
+  $('.header-line__content').addClass('header-line__content_scrolled');
+  $('.site-sections').addClass('site-sections_scrolled');
+  $('.top-line').addClass('top-line_scrolled');
+  $('.header-line__logo').addClass('d-none');
+  $('.header-line__logo_scrolled').removeClass('d-none');
 };
 
 const removeScrollHeader = () => {
   $('.header').removeClass('header_scrolled');
-  console.log('hello');
+  $('.header__content').removeClass('header__content_scrolled');
+  $('.phone-block__description').removeClass('d-none');
+  $('.basket__description').removeClass('d-none');
+  $('.burger-menu').removeClass('d-block');
+  $('.header-line__content').removeClass('header-line__content_scrolled');
+  $('.site-sections').removeClass('site-sections_scrolled');
+  $('.top-line').removeClass('top-line_scrolled');
+  $('.header-line__logo').removeClass('d-none');
+  $('.header-line__logo_scrolled').addClass('d-none');
 };
 
 const toggleScrollHeader = () => {
