8abe7ea84e411920d2a838c322eab68c66ce44ad
Dima Denisov <2denisov@bk.ru>
2019-07-19 01:41:58 +0300

    Новая сборка

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -150,3 +150,25 @@ var toggleScrollHeader = function toggleScrollHeader() {
 $('.burger-menu__checkbox').click(toggleBurgerMenu);
 $('.site-sections__link:not(:only-child').click(toggleSubMenuState);
 $(window).scroll(toggleScrollHeader);
+var heroSwiper = new Swiper('.hero-swiper', {
+  slideClass: 'hero-swiper__slide',
+  slidesPerView: 1,
+  spaceBetween: 0,
+  navigation: {
+    prevEl: '.button_icon_prev_gray',
+    nextEl: '.button_icon_next_gray'
+  },
+  pagination: {
+    el: '.pagination',
+    bulletElement: 'div',
+    bulletClass: 'pagination__item',
+    bulletActiveClass: 'pagination__item_active'
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true
+  },
+  observer: true,
+  observerParents: true,
+  observeSlideChildren: true
+});
