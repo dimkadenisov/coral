787ed9b87cf13f7c2b89c490b0ac1ba7b9ffb8ec
Dima Denisov <2denisov@bk.ru>
2019-08-12 17:18:58 +0300

    Реализация блока тэгов в каталоге

---
+++ frontend/scripts/components/tags.js
@@ -0,0 +1,9 @@
+$('.show-more-button').click(function() {
+  if ($(this).closest('.tags').hasClass('tags_show-all')) {
+    $(this).text('Показать еще');
+  } else {
+    $(this).text('Скрыть');
+  }
+
+  $(this).closest('.tags').toggleClass('tags_show-all')
+})
