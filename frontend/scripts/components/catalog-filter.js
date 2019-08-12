802c178a2e25a8d8d30b272d469b0bfa3c0b22bf
Dima Denisov <2denisov@bk.ru>
2019-08-13 00:16:42 +0300

    Реализован блок catalog-filter

---
+++ frontend/scripts/components/catalog-filter.js
@@ -0,0 +1,19 @@
+$('.filter-row-trigger').click(function(e) {
+  e.preventDefault();
+  if ($(window).width() > 991) $(this).closest('.filter-row').toggleClass('active');
+});
+
+(function() {
+  let timer;
+
+  window.onresize = () => {
+    if (timer) clearTimeout(timer);
+
+    if ($(window).width() > 991) {
+      timer = setTimeout(() => {
+        $('#catalog-filter').css('display', 'block');
+      }, 200)
+    }
+  };
+
+})();
