68052cfbe8cff7f4e7db5e15d0f86c1d87d6fb86
Dima Denisov <2denisov@bk.ru>
2019-08-20 02:51:35 +0300

    Кнопка «больше характеристик» теперь ссылка-якорь

--- frontend/scripts/components/item-card.js
+++ frontend/scripts/components/item-card.js
@@ -6,19 +6,6 @@ $('.item-card .button_heart').click(function() {
   }
 });
 
-$('.item-card__show-more-button').click(function() {
-  const characteristicksTable = $(this).closest('.item-card').find('.item-characteristics');
-  if (characteristicksTable.hasClass('item-characteristics_opened')) {
-    $(this).text('Больше характеристик');
-    $(this).removeClass('button_gray');
-    characteristicksTable.removeClass('item-characteristics_opened');
-  } else {
-    $(this).text('Меньше характеристик');
-    $(this).addClass('button_gray');
-    characteristicksTable.addClass('item-characteristics_opened');
-  }
-})
-
 $('.item-card .buy-button').click(function() {
   ($(this).hasClass('buy-button_clicked'))
     ? $(this).find('.button__text').text('Удалить из\u00A0корзины')
