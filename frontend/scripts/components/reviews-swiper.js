fac3b6cdb2085d0921cdbaca31f4c2c3f7141bf0
Dima Denisov <2denisov@bk.ru>
2019-08-20 20:34:26 +0300

    Рефакторинг обновления свайпера отзывов
    Теперь за апдейт свайпера отзывов при ресайзе отвечает
    отдельная функция updateSwiperOnBreakpoint

--- frontend/scripts/components/reviews-swiper.js
+++ frontend/scripts/components/reviews-swiper.js
@@ -1,69 +1,49 @@
-(function() {
-  if ($('.reviews-swiper').length === 0) return false;
+const reviewsSwiperConfig = {
+  slideClass: 'review',
+  slidesPerView: 2,
+  slidesPerColumn: 2,
+  spaceBetween: 30,
 
-  const breakpoints = [window.matchMedia( '(max-width: 991px)' ), window.matchMedia( '(min-width: 992px)' )];
+  allowTouchMove: false,
 
-  const reviewsSwiperConfig = {
-    slideClass: 'review',
-    slidesPerView: 2,
-    slidesPerColumn: 2,
-    spaceBetween: 30,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
 
-    allowTouchMove: false,
+  observer: true,
+  observerParents: true,
+  observeSlideChildren: true,
 
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true,
-    },
+  breakpoints: {
+    991: {
+      slideClass: 'review',
+      slidesPerView: 1,
+      slidesPerColumn: 1,
+      spaceBetween: 30,
 
-    observer: true,
-    observerParents: true,
-    observeSlideChildren: true,
+      allowTouchMove: true,
 
-    breakpoints: {
-      991: {
-        slideClass: 'review',
-        slidesPerView: 1,
-        slidesPerColumn: 1,
-        spaceBetween: 30,
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
 
-        allowTouchMove: true,
+      observer: true,
+      observerParents: true,
+      observeSlideChildren: true,
 
-        keyboard: {
-          enabled: true,
-          onlyInViewport: true,
-        },
-
-        observer: true,
-        observerParents: true,
-        observeSlideChildren: true,
-
-        pagination: {
-          el: '.pagination',
-          bulletElement: 'div',
-          bulletClass: 'pagination__item',
-          bulletActiveClass: 'pagination__item_active',
-        },
+      pagination: {
+        el: '.pagination',
+        bulletElement: 'div',
+        bulletClass: 'pagination__item',
+        bulletActiveClass: 'pagination__item_active',
       },
     },
-  };
+  },
+};
 
-  let reviewsSwipers = generateSwipers('reviews-swiper', reviewsSwiperConfig);
+// const reviewsSwiperBreakpoints = [window.matchMedia( '(max-width: 991px)' ), window.matchMedia( '(min-width: 992px)' )];
+const reviewsSwiperBreakpoints = [991];
 
-  const breakpointChecker = function() {
-    breakpoints.forEach(breakpoint => {
-      if (breakpoint.matches) {
-        reviewsSwipers.forEach(item => {
-          item.destroy();
-        });
-        reviewsSwipers = generateSwipers('reviews-swiper', reviewsSwiperConfig);
-      }
-    })
-  };
-
-  for (let i = 0; i < breakpoints.length; i++) {
-    const breakpoint = breakpoints[i];
-    breakpoint.addListener(breakpointChecker);
-  }
-  breakpointChecker();
-})();
+let reviewsSwipers = updateSwiperOnBreakpoint('reviews-swiper', reviewsSwiperConfig, reviewsSwiperBreakpoints);
