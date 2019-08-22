0bc930569aecae14be5abf78d15823788326ef5a
Dima Denisov <2denisov@bk.ru>
2019-08-22 22:20:13 +0300

    Реализация страниц «статьи» и «статья»

---
+++ frontend/scripts/components/series-swiper.js
@@ -0,0 +1,52 @@
+const seriesSwiperConfig = {
+  slideClass: 'series-item',
+  slidesPerView: 4,
+  spaceBetween: 30,
+  allowTouchMove: false,
+
+  watchOverflow: true,
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  navigation: {
+    prevEl: '.prev',
+    nextEl: '.next',
+  },
+
+  pagination: {
+    el: '.pagination',
+    bulletElement: 'div',
+    bulletClass: 'pagination__item',
+    bulletActiveClass: 'pagination__item_active',
+  },
+
+  observer: true,
+  observerParents: true,
+  observeSlideChildren: true,
+
+  breakpoints: {
+    1343: {
+      slidesPerView: 3,
+    },
+
+    991: {
+      slidesPerView: 3,
+    },
+
+    767: {
+      slidesPerView: 2,
+      allowTouchMove: true,
+    },
+
+    575: {
+      slidesPerView: 1,
+      allowTouchMove: true,
+    },
+  },
+};
+
+let seriesSwipers = generateSwipers({swiperClass: 'series-swiper', swiperConfig: seriesSwiperConfig});
+console.log(seriesSwipers);
