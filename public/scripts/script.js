07289e737e1dafbcd4b334fa6d3ac0b6fea9c4ec
Dima Denisov <2denisov@bk.ru>
2019-09-14 00:35:35 +0300

    Новая сборка

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -490,6 +490,10 @@ function generateItemsSwipers() {
 }
 
 ;
+$('.licensed-collections__button-wrapper .button').click(function () {
+  $(this).closest('.licensed-collections').addClass('licensed-collections_opened');
+  $(this).remove();
+});
 
 function generateNewArrrivalsSwiper(thumbs) {
   var newArrivalsSwiperConfig = {
